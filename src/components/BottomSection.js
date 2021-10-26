8c3cd27d1e1b734960a6cdb5b995d3b308e30d68
Oleksandr Boitsun <thewip3out@gmail.com>
2021-10-26 19:38:57 +0300

    some sections added

--- src/components/BottomSection.js
+++ src/components/BottomSection.js
@@ -1,10 +1,31 @@
 import React from "react";
-
+import "../styles/bottom-section.scss";
 export default function BottomSection() {
   return (
-    <div>
-      <div className="my-container">
-        <img className="w-full" src="/imgs/squad-bottom.png" alt="squaaaaad" />
+    <div className="pt-20 relative bottom-section">
+      <div className="my-container relative bottom-20">
+        <img
+          className="rounded-xl overflow-hidden block w-full shadow-xl relative  z-10"
+          src="/imgs/squad-bottom.png"
+          alt="squaaaaad"
+        />{" "}
+        <img className="other-cards-l-1 other-cards" src="/imgs/card1.png" alt="card" />
+        <img
+          className="other-cards-l-3 other-cards"
+          src="/imgs/card3.png"
+          alt="card"
+        />{" "}
+        <img className="other-cards-l-2 other-cards" src="/imgs/card7.png" alt="card" />
+        <img className="other-cards-r-2 other-cards r" src="/imgs/card5.png" alt="card" />
+        <img className="other-cards-r-3 other-cards r" src="/imgs/card6.png" alt="card" />
+        <img className="other-cards-r-1 other-cards r" src="/imgs/card4.png" alt="card" />
+      </div>
+      <div className="bottom-section-net">
+        <img
+          className=" w-full absolute  object-cover "
+          src="/imgs/bottom-bg.png"
+          alt="bg"
+        />
       </div>
     </div>
   );

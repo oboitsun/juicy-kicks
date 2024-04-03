7a1f7710f19269e9886b4b577cd0e5b196f3a4cf
oboitsun <thewip3out@gmail.com>
2024-04-03 13:30:01 +0300

    legacy coe updates

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,7 +4,7 @@ import TimerCountItem from "./TimerCountItem";
 import { calculateTimeLeft } from "./utils";
 export default function Timer() {
   const today = Date.now();
-  const dayX = new Date("October 28, 2024 00:00:00").getTime();
+  const dayX = new Date("July 28, 2024 00:00:00").getTime();
   const [timeLeft, setTimeLeft] = useState({
     hours: "0",
     days: "0",
